efcb2f5d73a6619dbac64efb72f159a3d80efb0e
RSrujanaa <srujana.r@capgemini.com>
2024-12-30 16:35:23 +0530

    first python script file

---
+++ grades.py
@@ -0,0 +1,12 @@
+marks = int(input("enter marks"))
+
+if (marks>=90):
+    print("grade A")
+elif (marks >=80 and marks<90):
+    print("grade B")
+elif (marks >=70 and marks < 80):
+    print("Grade c")
+elif (marks < 70):
+    print("grade D")
+else:
+    print("wrong marks")
